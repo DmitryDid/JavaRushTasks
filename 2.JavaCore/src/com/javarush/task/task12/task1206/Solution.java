e1dda4db5a45ba779c268e1c254d0ab6d2d9e0f8
Dmitry <Dmitry@example.com>
2018-05-06 00:34:15 +0700

    level 12+

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+//        Solution.print(2);
+    }
+
+    public static void print(int i){}//Напишите тут ваши методы
+    public static void print(String i){}//Напишите тут ваши методы
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+//        System.out.println(max(52,100));
+    }
+    
+    public static int max(int a, int b){
+        return a < b ? b : a;
+    }
+
+    public static long max(long a, long b){
+        return a < b ? b : a;
+    }
+
+    public static double max(double a, double b){
+        return a < b ? b : a;
+    }
+    //Напишите тут ваши методы
+}
