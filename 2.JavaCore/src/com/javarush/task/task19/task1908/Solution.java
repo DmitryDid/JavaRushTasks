fb8572f2711307820e4a064eb9129a8782ab7c3b
Dididid <sdf1vsd6341KH65df>
2018-09-14 17:39:22 +0700

    1911

--- 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1908/Solution.java
@@ -4,7 +4,28 @@ package com.javarush.task.task19.task1908;
 Выделяем числа
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(reader.readLine()));
+//        BufferedReader fileReader = new BufferedReader(new FileReader("dataIn.txt"));
+//        BufferedWriter fileWriter = new BufferedWriter(new FileWriter("dataOut.txt"));
+        reader.close();
+//        long time = System.currentTimeMillis();
+        while (fileReader.ready()) {
+            String[] strings = fileReader.readLine().split(" ");
+            for (String s : strings) {
+                try {
+                    fileWriter.write(Integer.parseInt(s) + " ");
+                } catch (NumberFormatException e) {
+                }
+            }
+        }
+//        System.out.println(System.currentTimeMillis() - time);
+        fileReader.close();
+        fileWriter.close();
     }
 }
